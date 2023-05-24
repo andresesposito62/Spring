f054659fa72da6bdf499a9157ae1767b4cadd411
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2023-05-23 19:44:05 -0500

    Repository annotation is implemented

--- src/main/java/com/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -2,13 +2,36 @@ package com.platzimarket.persistence;
 
 import com.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(Long idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(Long cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
--- src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,10 @@ package com.platzimarket.persistence.crud;
 import com.platzimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Long idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Long cantidadStock, boolean estado);
 }
