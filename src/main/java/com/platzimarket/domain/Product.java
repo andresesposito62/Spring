ba05427b87fa8ee3874ddfb3b7b0a781963f2495
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2023-05-25 20:03:29 -0500

    ProductRepository is implemented

---
+++ src/main/java/com/platzimarket/domain/Product.java
@@ -0,0 +1,70 @@
+package com.platzimarket.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long productId;
+    private String name;
+    private Long categoryId;
+    private BigDecimal price;
+    private Long stock;
+    private Boolean active;
+    private Category category;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
